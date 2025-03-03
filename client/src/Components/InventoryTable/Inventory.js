c60244a6af0ef752a89838c461b6fa44e883242e
Ragnalex <bernardo.millapan@gmail.com>
2025-03-03 18:00:38 -0300

    Actualizacion tabla

--- client/src/Components/InventoryTable/Inventory.js
+++ client/src/Components/InventoryTable/Inventory.js
@@ -6,35 +6,66 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { DataGrid } from '@mui/x-data-grid';
+
 import './Inventory.css';
 
+
+const columns = [
+  { field: 'barcode', headerName: 'Código Barras', width: 150 },
+  {
+    field: 'id',
+    headerName: 'Código Proveedor',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'name',
+    headerName: 'Descripción',
+    width: 500,
+    editable: true,
+  },
+  {
+    field: 'expire_date',
+    headerName: 'Fecha Expiración',
+    width: 130,
+    editable: true,
+  },
+  {
+    field: 'quantity',
+    headerName: 'Cantidad',
+    type: 'number',
+    editable: true,
+    width: 100,
+  },
+];
 
 
 const Testrows = [
   {
     barcode: 123456,
-    supplier_code: "908100020",
+    id: "908100020",
     name: "ESTILETE ENDOTRAQUEAL FR 10 (M)",
     expire_date: "2024-12-31",
     quantity: 70
   },
   {
     barcode: 456789,
-    supplier_code: "102150010",
+    id: "102150010",
     name: "MASCARA LARINGEA LMA PROSEAL Nº1 R",
     expire_date: "2025-01-05",
     quantity: 130
   },
   {
     barcode: 4587954,
-    supplier_code: "2407043100",
+    id: "2407043100",
     name: "ROLLO TYVEK 10CMX70MT STERITEK",
     expire_date: "",
     quantity: 35
   },
   {
     barcode: 54658912,
-    supplier_code: "800HDP",
+    id: "800HDP",
     name: "POSICIONADOR DE CABEZA VITAL SIGNS DESECH CJX6 UD",
     expire_date: "",
     quantity: 5
@@ -45,34 +76,16 @@ const Testrows = [
 const InventoryMain = () => {
 
   return (
-   <div>
-    <TableContainer>
-      <TableHead>
-        <TableRow>
-          <TableCell></TableCell>
-          <TableCell className='headerText'>Código barras</TableCell>
-          <TableCell className='headerText'>Código proveedor</TableCell>
-          <TableCell className='headerText'>Descripción</TableCell>
-          <TableCell className='headerText'>Fecha vencimiento</TableCell>
-          <TableCell className='headerText'>Cantidad</TableCell>
-          <TableCell></TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {Testrows.map((row, index) => (
-          <TableRow key={index}>
-            <TableCell></TableCell>
-            <TableCell>{row.barcode}</TableCell>
-            <TableCell>{row.supplier_code}</TableCell>
-            <TableCell>{row.name}</TableCell>
-            <TableCell>{row.expire_date}</TableCell>
-            <TableCell>{row.quantity}</TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </TableContainer>
-   </div>
+    <div>
+      <div style={{ height: 400, width: '100%' }}>
+          <DataGrid
+            rows={Testrows}
+            columns={columns}
+            pageSize={5}
+            disableSelectionOnClick
+          />
+        </div>
+    </div>
   )
 }
 
