49cadc91ace2577d0cd300d6dcd289eabf1df101
Ragnalex <bernardo.millapan@gmail.com>
2025-03-04 16:39:33 -0300

    Update insumos.controllers.js

--- serv/controllers/insumos.controllers.js
+++ serv/controllers/insumos.controllers.js
@@ -6,7 +6,7 @@ const getInsumos = async (req, res) => {
     const response = await pool.query("SELECT * FROM InventarioInsumos INNER JOIN Insumo ON InventarioInsumos.id_insumo = Insumo.id_insumo")
     res.status(200).json(response.rows)
   }
-  catch{
+  catch(error){
     res.status(500).json(error)
   }
 
